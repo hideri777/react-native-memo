154b1a921a890a58641cd876b6d79fe218f28849
hideri <hori.hide1003@gmail.com>
2021-03-30 21:25:49 +0900

    リストの表示

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Title } from "react-native-paper";
+import { StyleSheet, View, FlatList } from "react-native";
+import { List } from "react-native-paper";
+
+const memos = [
+  {
+    text: "メモメモメモ",
+    createdAt: 1585574700000, //2020.03.3022:25
+  },
+  {
+    text: "メモメモメモ",
+    createdAt: 1585567500000, //2020.03.3020:25
+  },
+  {
+    text: "長いメモメモメモメモメモメモメモメモメモメモメモメモメモメモ",
+    createdAt: 1585459500000, //2020.03.2914:25
+  },
+  {
+    text: "メモメモメモ",
+    createdAt: 1585369500000, //2020.03.2813:25
+  },
+  {
+    text: "メモメモメモ",
+    createdAt: 1585275900000, //2020.03.2711:25
+  },
+];
 
 export const MainScreen = () => {
   return (
     <View style={styles.container}>
-      <Title>ここはメイン画面です</Title>
+      <FlatList
+        style={styles.list}
+        data={memos}
+        keyExtractor={(item) => `${item.createdAt}`}
+        renderItem={({ item }) => (
+          <List.Item title={item.text} description={item.createdAt} />
+        )}
+      />
     </View>
   );
 };
@@ -13,8 +43,8 @@ export const MainScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  list: {
+    flex: 1,
   },
 });
