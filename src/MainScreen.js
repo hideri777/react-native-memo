0a94bb99762c7696d6b256aeed4eb1487ac3199b
hideri <hori.hide1003@gmail.com>
2021-03-31 09:17:47 +0900

    見た目整形

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 import { List } from "react-native-paper";
+import format from "date-fns/format";
 
 const memos = [
   {
@@ -33,7 +34,15 @@ export const MainScreen = () => {
         data={memos}
         keyExtractor={(item) => `${item.createdAt}`}
         renderItem={({ item }) => (
-          <List.Item title={item.text} description={item.createdAt} />
+          <List.Item
+            title={item.text}
+            titleNumberOfLines={5}
+            description={`作成日時: ${format(
+              item.createdAt,
+              "yyyy.MM.dd HH:mm"
+            )}`}
+            descriptionStyle={{ textAlign: "right" }}
+          />
         )}
       />
     </View>
